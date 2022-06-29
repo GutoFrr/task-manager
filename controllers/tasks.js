b0fd154686f2f3cb95813a0adfcc73f8984db5f2
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-06-29 13:01:40 -0300

    createTask requirement added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,12 +3,18 @@ const getAllTasks = (req, res) => {
   res.send('get all tasks')
 }
 
-const createTask = (req, res) => {
-  res.json(req.body)
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+
 }
 
 const getTask = (req, res) => {
-  res.json({id:req.params.id})
+  res.json({ id: req.params.id })
 }
 
 const updateTask = (req, res) => {
